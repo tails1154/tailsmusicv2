0a12f232bb79b2bb638584ffd357ad596d23b4df
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-06-25 14:17:43 -0500

    update push.sh

--- push.sh
+++ push.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-./docs.sh
+#./docs.sh
 git init
-git add .
 echo "Enter commit message"
 read -p ">" COMMITMSG
+./docs.sh
+git add .
 git commit -m "$COMMITMSG"
-git push
+echo "Would you like to push now? (Y/n)"
+read -p "push>" YESNO
+if [ "$YESNO" = "n" ]; then
+	exit 0
+else
+	git push
+fi
