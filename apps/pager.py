040c098f3e2f701674a995279e4f85b0bb8e4a6c
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-06-25 19:40:39 -0500

    append self. to speak function in apps/pager.py

--- apps/pager.py
+++ apps/pager.py
@@ -10,7 +10,7 @@ class speaker:
  def __init__(self, queue):
   self.queue = queue
  def speak(self, text):
-  queue.put("espeak-ng " + text)
+  self.queue.put("espeak-ng " + text)
 class PagerClient:
     def __init__(
         self,
